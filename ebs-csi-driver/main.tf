0b80a3d06fcc087963ab521dbecf4b1ec56c0aba
karty11 <158238682+karty11@users.noreply.github.com>
2025-09-24 23:39:39 +0530

    Update main.tf

--- ebs-csi-driver/main.tf
+++ ebs-csi-driver/main.tf
@@ -17,10 +17,8 @@ data "tls_certificate" "oidc" {
   url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
 
-resource "aws_iam_openid_connect_provider" "eks" {
-  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.oidc.certificates[0].sha1_fingerprint]
+data "aws_iam_openid_connect_provider" "eks" {
+  arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.oidc_provider_url}"
 }
 
 # --- IAM Role for EBS CSI Driver ---
